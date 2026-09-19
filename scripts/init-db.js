79062947ba0d8ee9cfb0d2a85960c573288fa065
Zaiwaa <105898431+Zaiwaa@users.noreply.github.com>
2026-09-19 21:48:58 +0800

    Delete scripts directory

--- scripts/init-db.js
+++
@@ -1,3 +0,0 @@
-require('dotenv').config();
-const { initDb, pool } = require('../src/db');
-initDb().then(() => { console.log('Database initialized.'); return pool.end(); }).catch(err => { console.error(err); process.exit(1); });
